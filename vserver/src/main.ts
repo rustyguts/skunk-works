2510c0e3d6598cc1360dc06dd1d8110919084dc5
Brendan Kennedy <brenwken@gmail.com>
2023-07-25 13:56:19 +0000

    Add Sentry

--- vserver/src/main.ts
+++ vserver/src/main.ts
@@ -1,13 +1,22 @@
-import * as morgan from 'morgan';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
+import * as Sentry from '@sentry/node';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('combined'));
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN || '',
+
+    // Performance Monitoring
+    tracesSampleRate: 0.2, // Capture 100% of the transactions, reduce in production!
+  });
+
   await app.listen(5000);
 }
 bootstrap();
